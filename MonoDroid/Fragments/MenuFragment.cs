d739f8b232610bc5d335609f0f1046048f675825
beginor <beginor@qq.com>
2013-01-22 08:57:34 +0800

    change to fragment

--- MonoDroid/Fragments/MenuFragment.cs
+++ MonoDroid/Fragments/MenuFragment.cs
@@ -1,5 +1,6 @@
 using Android.Widget;
 using MonoDroid.Attributes;
+using Android.Views;
 
 namespace MonoDroid.Fragments {
 
@@ -9,9 +10,19 @@ namespace MonoDroid.Fragments {
 			base.OnStart();
 			var activity = this.Activity;
 			this.ListAdapter = new SampleAdapter(activity);
+			/*
 			var listener = activity as ListView.IOnItemClickListener;
 			if (listener != null) {
 				this.ListView.OnItemClickListener = listener;
+			}
+			*/
+		}
+
+		public override void OnListItemClick(ListView listView, View view, int position, long id) {
+			base.OnListItemClick(listView, view, position, id);
+			var sample = view.Tag as Sample;
+			if (sample != null) {
+				MainActivity.Current.ShowSample(sample);
 			}
 		}
 	}
--- MonoDroid/MainActivity.cs
+++ MonoDroid/MainActivity.cs
@@ -2,7 +2,6 @@ using System;
 
 using Android.App;
 using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
@@ -12,12 +11,19 @@ using MonoDroid.Fragments;
 namespace MonoDroid {
 
 	[Activity (Label = "@string/app_name", MainLauncher = true)]
-	public class MainActivity : Android.Support.V4.App.FragmentActivity, ListView.IOnItemClickListener {
+	public class MainActivity : Android.Support.V4.App.FragmentActivity {
 
 		private MenuFragment _menuFrag;
 
+		public static MainActivity Current {
+			get;
+			private set;
+		}
+
 		protected override void OnCreate(Bundle bundle) {
 			base.OnCreate(bundle);
+
+			Current = this;
 
 			this.SetContentView(Resource.Layout.activity_main);
 
@@ -29,30 +35,24 @@ namespace MonoDroid {
 			trans.Commit();
 		}
 
-		public void OnItemClick(AdapterView parent, View view, int position, long id) {
-			var sample = view.Tag as Sample;
-			if (sample != null) {
-				var fragmentType = typeof(Android.Support.V4.App.Fragment);
-				if (fragmentType.IsAssignableFrom(sample.Launch)) {
-					var frag = Activator.CreateInstance(sample.Launch) as Android.Support.V4.App.Fragment;
-					this.PushFragment(frag);
-				}
-				else {
-					var intent = new Intent(this, sample.Launch);
-					this.StartActivity(intent);
-				}
-			}
-			else {
-				Toast.MakeText(this, "Error: Sample is empty!", ToastLength.Short);
-			}
-		}
-
 		public void PushFragment(Android.Support.V4.App.Fragment frag) {
 			var fm = this.SupportFragmentManager;
 			var trans = fm.BeginTransaction();
 			trans.Replace(Resource.Id.activity_main_content_frame_layout, frag);
 			trans.AddToBackStack(null);
 			trans.Commit();
+		}
+
+		public void ShowSample(Sample sample) {
+			var fragmentType = typeof(Android.Support.V4.App.Fragment);
+			if (fragmentType.IsAssignableFrom(sample.Launch)) {
+				var frag = Activator.CreateInstance(sample.Launch) as Android.Support.V4.App.Fragment;
+				this.PushFragment(frag);
+			}
+			else {
+				var intent = new Intent(this, sample.Launch);
+				this.StartActivity(intent);
+			}
 		}
 
 	}
